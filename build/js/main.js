7c05939c480d778d5484fb4b523d4d7e5f8759e8
Andrii Husach <andrii.husach@gmail.com>
2017-11-22 23:51:56 +0200

    'correct'

--- build/js/main.js
+++ build/js/main.js
@@ -1,35 +1,47 @@
 window.onload = function () {
 
-	var open = document.getElementById("menu-open");
-	var closed = document.getElementById("menu-closed");
-	var menu = document.getElementById("main-nav");
-	var headerToggle = document.getElementById("header-toggle");
-	var menuLink = document.getElementsByClassName("menu-link");
-	var search = document.getElementById("form__search");
-	var headerMagnify = document.getElementById("header__magnify")
+  var open = document.querySelector("#menu-open");
+  var closed = document.querySelector("#menu-closed");
+  var menu = document.querySelector("#main-nav");
+  var headerToggle = document.querySelector("#header-toggle"); 
+  var search = document.querySelector("#form__search");
+  var headerMagnify = document.querySelector("#header__magnify");
+  var headerSearch = document.querySelector("#header__search");
+  var formOk = document.querySelector("#form__ok");
+  var formClosed = document.querySelector("#form__closed");
+  var menuLink = document.querySelector(".menu-link");
 
-	open.addEventListener("click", 
-	  function () {
-		  menu.classList.toggle("main-nav--hidden");	
-		  open.classList.add("header-toggle__img--hidden");
-		  headerToggle.classList.add("header-toggle--big");
-		  closed.classList.remove("header-toggle__img--hidden");
-		  menuLink.classList.add("menu__link--open");
-		}, false);
+  open.addEventListener("click", 
+    function () {
+      menu.classList.toggle("main-nav--hidden");
+      open.classList.add("header-toggle__img--hidden");
+      headerToggle.classList.add("header-toggle--big");
+      closed.classList.remove("header-toggle__img--hidden");
+      menuLink.classList.toggle("menu__link--open");
+    }, false);
 
-	closed.addEventListener("click", 
-	  function () {
-		  menu.classList.toggle("main-nav--hidden");	
-		  closed.classList.add("header-toggle__img--hidden");
-		  open.classList.remove("header-toggle__img--hidden");		  
-		  headerToggle.classList.remove("header-toggle--big");
-		  menuLink.classList.remove("menu__link--open");
-		}, false);
+  closed.addEventListener("click", 
+    function () {
+      menu.classList.toggle("main-nav--hidden");	
+      closed.classList.add("header-toggle__img--hidden");
+      open.classList.remove("header-toggle__img--hidden");		  
+      headerToggle.classList.remove("header-toggle--big");
+      menuLink.classList.toggle("menu__link--open");
+	}, false);
 		
-	search.onmouseover = function() {
-		headerMagnify.classList.toggle("header__magnify--hidden");
-	}
-	search.onmouseout = function() {
-		headerMagnify.classList.toggle("header__magnify--hidden");
-	}
+  search.onfocus = function() {
+    headerMagnify.classList.toggle("header__magnify--hidden");
+  }
+  search.onblur = function() {
+    headerMagnify.classList.toggle("header__magnify--hidden");
+  }
+  headerSearch.onfocus = function() {
+    formOk.classList.toggle("form__ok--hidden");
+    formClosed.classList.toggle("form__closed--hidden");
+  }
+  headerSearch.onblur = function() {
+    formOk.classList.toggle("form__ok--hidden");
+    formClosed.classList.toggle("form__closed--hidden");
+  }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 window.onload = function () {
 
-  var open = document.getElementById("menu-open");
-  var closed = document.getElementById("menu-closed");
-  var menu = document.getElementById("main-nav");
-  var headerToggle = document.getElementById("header-toggle");
-  var menuLink = document.getElementsByClassName("menu-link");
-  var search = document.getElementById("form__search");
-  var headerMagnify = document.getElementById("header__magnify");
-  var headerSearch = document.getElementById("header__search");
-  var formOk = document.getElementById("form__ok");
-  var formClosed = document.getElementById("form__closed");
+  var open = document.querySelector("#menu-open");
+  var closed = document.querySelector("#menu-closed");
+  var menu = document.querySelector("#main-nav");
+  var headerToggle = document.querySelector("#header-toggle"); 
+  var search = document.querySelector("#form__search");
+  var headerMagnify = document.querySelector("#header__magnify");
+  var headerSearch = document.querySelector("#header__search");
+  var formOk = document.querySelector("#form__ok");
+  var formClosed = document.querySelector("#form__closed");
+  var menuLink = document.querySelector(".menu-link");
 
   open.addEventListener("click", 
     function () {
@@ -29,17 +29,17 @@ window.onload = function () {
       menuLink.classList.toggle("menu__link--open");
 	}, false);
 		
-  search.onmouseover = function() {
+  search.onfocus = function() {
     headerMagnify.classList.toggle("header__magnify--hidden");
   }
-  search.onmouseout = function() {
+  search.onblur = function() {
     headerMagnify.classList.toggle("header__magnify--hidden");
   }
-  headerSearch.onmouseover = function() {
+  headerSearch.onfocus = function() {
     formOk.classList.toggle("form__ok--hidden");
     formClosed.classList.toggle("form__closed--hidden");
   }
-  headerSearch.onmouseout = function() {
+  headerSearch.onblur = function() {
     formOk.classList.toggle("form__ok--hidden");
     formClosed.classList.toggle("form__closed--hidden");
   }
