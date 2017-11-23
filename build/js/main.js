fdae171731da59fb8b70c06c51de47c4e00e86af
Andrii Husach <andrii.husach@gmail.com>
2017-11-23 12:13:01 +0200

    'desktop_menu'

--- build/js/main.js
+++ build/js/main.js
@@ -12,27 +12,31 @@ window.onload = function () {
   var headerMagnify = document.querySelector("#header__magnify"); 
   var toDo = document.querySelector("#text-to");
 
-  btnOpen.addEventListener("click", 
-    function () {
-      menu.classList.toggle("main-nav--hidden");
-      btnOpen.classList.toggle("header-toggle__img--hidden");
-      headerToggle.classList.add("header-toggle--big");
-      closed.classList.remove("header-toggle__img--hidden");
-      toDo.classList.toggle("header-toggle__text--to");
-      menuLink.classList.toggle("menu__link--open");
-      
-    }, false);
+  if (window.innerWidth < 1440) {
 
-  closed.addEventListener("click", 
-    function () {
-      menu.classList.toggle("main-nav--hidden");	
-      closed.classList.add("header-toggle__img--hidden");
-      btnOpen.classList.toggle("header-toggle__img--hidden");
-      headerToggle.classList.remove("header-toggle--big");
-      toDo.classList.toggle("header-toggle__text--to");
-      menuLink.classList.toggle("menu__link--open");
-      
-	}, false);
+    btnOpen.addEventListener("click", 
+      function () {
+        menu.classList.toggle("main-nav--hidden");
+        btnOpen.classList.toggle("header-toggle__img--hidden");
+        headerToggle.classList.add("header-toggle--big");
+        closed.classList.remove("header-toggle__img--hidden");
+        toDo.classList.toggle("header-toggle__text--to");
+        menuLink.classList.toggle("menu__link--open");
+        
+      }, false);
+
+    closed.addEventListener("click", 
+      function () {
+        menu.classList.toggle("main-nav--hidden");	
+        closed.classList.add("header-toggle__img--hidden");
+        btnOpen.classList.toggle("header-toggle__img--hidden");
+        headerToggle.classList.remove("header-toggle--big");
+        toDo.classList.toggle("header-toggle__text--to");
+        menuLink.classList.toggle("menu__link--open");
+        
+  	}, false);
+
+  }
 		
 /*  mainSearch.addEventListener("onfocus", 
     function () {
