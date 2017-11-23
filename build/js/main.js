5fa7b165a8f40eec8f08acf478abc1c880603e85
Andrii Husach <andrii.husach@gmail.com>
2017-11-23 11:51:43 +0200

    'pixel_perfect'

--- build/js/main.js
+++ build/js/main.js
@@ -1,38 +1,53 @@
 window.onload = function () {
-
-  var open = document.querySelector("#menu-open");
-  var closed = document.querySelector("#menu-closed");
+  
   var menu = document.querySelector("#main-nav");
-  var headerToggle = document.querySelector("#header-toggle"); 
-  var search = document.querySelector("#form__search");
-  var headerMagnify = document.querySelector("#header__magnify");
-  var headerSearch = document.querySelector("#header__search");
+  var closed = document.querySelector("#menu-closed");
   var formOk = document.querySelector("#form__ok");
-  var formClosed = document.querySelector("#form__closed");
+  var btnOpen = document.querySelector("#menu-open");
   var menuLink = document.querySelector(".menu-link");
+  var formClosed = document.querySelector("#form__closed");
+  var mainSearch = document.querySelector("#main-search");
+  var headerSearch = document.querySelector("#header__search");
+  var headerToggle = document.querySelector("#header-toggle"); 
+  var headerMagnify = document.querySelector("#header__magnify"); 
+  var toDo = document.querySelector("#text-to");
 
-  open.addEventListener("click", 
+  btnOpen.addEventListener("click", 
     function () {
       menu.classList.toggle("main-nav--hidden");
-      open.classList.add("header-toggle__img--hidden");
+      btnOpen.classList.toggle("header-toggle__img--hidden");
       headerToggle.classList.add("header-toggle--big");
       closed.classList.remove("header-toggle__img--hidden");
+      toDo.classList.toggle("header-toggle__text--to");
       menuLink.classList.toggle("menu__link--open");
+      
     }, false);
 
   closed.addEventListener("click", 
     function () {
       menu.classList.toggle("main-nav--hidden");	
       closed.classList.add("header-toggle__img--hidden");
-      open.classList.remove("header-toggle__img--hidden");		  
+      btnOpen.classList.toggle("header-toggle__img--hidden");
       headerToggle.classList.remove("header-toggle--big");
+      toDo.classList.toggle("header-toggle__text--to");
       menuLink.classList.toggle("menu__link--open");
+      
 	}, false);
 		
-  search.onfocus = function() {
+/*  mainSearch.addEventListener("onfocus", 
+    function () {
+      headerMagnify.classList.toggle("header__magnify--hidden");
+  }, false);
+
+  mainSearch.addEventListener("onblur", 
+    function () {
+      headerMagnify.classList.toggle("header__magnify--hidden");
+  }, false);*/
+
+  mainSearch.onfocus = function() {
     headerMagnify.classList.toggle("header__magnify--hidden");
   }
-  search.onblur = function() {
+  mainSearch.onblur = function() {
     headerMagnify.classList.toggle("header__magnify--hidden");
   }
   headerSearch.onfocus = function() {
