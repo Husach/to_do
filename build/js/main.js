5b9954f13245b63cfe2f64355a6f2caa2b6a3608
Andrii Husach <andrii.husach@gmail.com>
2017-11-24 10:24:33 +0200

    'update_build'

--- build/js/main.js
+++ build/js/main.js
@@ -1,66 +1,49 @@
 window.onload = function () {
   
-  var menu = document.querySelector("#main-nav");
-  var closed = document.querySelector("#menu-closed");
-  var formOk = document.querySelector("#form__ok");
-  var btnOpen = document.querySelector("#menu-open");
-  var menuLink = document.querySelector(".menu-link");
-  var formClosed = document.querySelector("#form__closed");
-  var mainSearch = document.querySelector("#main-search");
-  var headerSearch = document.querySelector("#header__search");
-  var headerToggle = document.querySelector("#header-toggle"); 
-  var headerMagnify = document.querySelector("#header__magnify"); 
-  var toDo = document.querySelector("#text-to");
-
+  var btnOpen = document.querySelector("#menu-open"),
+      btnClosed = document.querySelector("#menu-closed"),
+      menu = document.querySelector("#main-nav"),
+      toDo = document.querySelector("#text-to"),
+      headerToggle = document.querySelector("#header-toggle"),
+      mainSearch = document.querySelector("#main-search"),
+      Magnify = document.querySelector("#header__magnify"),
+      formOk = document.querySelector("#form__ok"),
+      formClosed = document.querySelector("#form__closed"),
+      headerSearch = document.querySelector("#header__search");
+  
   if (window.innerWidth < 1440) {
-
-    btnOpen.addEventListener("click", 
-      function () {
-        menu.classList.toggle("main-nav--hidden");
-        btnOpen.classList.toggle("header-toggle__img--hidden");
-        headerToggle.classList.add("header-toggle--big");
-        closed.classList.remove("header-toggle__img--hidden");
-        toDo.classList.toggle("header-toggle__text--to");
-        menuLink.classList.toggle("menu__link--open");
-        
-      }, false);
-
-    closed.addEventListener("click", 
-      function () {
-        menu.classList.toggle("main-nav--hidden");	
-        closed.classList.add("header-toggle__img--hidden");
-        btnOpen.classList.toggle("header-toggle__img--hidden");
-        headerToggle.classList.remove("header-toggle--big");
-        toDo.classList.toggle("header-toggle__text--to");
-        menuLink.classList.toggle("menu__link--open");
-        
-  	}, false);
-
+    btnOpen.addEventListener("click", resizeMenu, false);
+    btnClosed.addEventListener("click", resizeMenu, false);
   }
-		
-/*  mainSearch.addEventListener("onfocus", 
-    function () {
-      headerMagnify.classList.toggle("header__magnify--hidden");
-  }, false);
 
-  mainSearch.addEventListener("onblur", 
-    function () {
-      headerMagnify.classList.toggle("header__magnify--hidden");
-  }, false);*/
+  if (window.innerWidth < 1440 && window.innerWidth > 1023) {
+    btnOpen.addEventListener("click", menuTablet, false);
+    btnClosed.addEventListener("click", menuTablet, false);
+  }
 
-  mainSearch.onfocus = function() {
-    headerMagnify.classList.toggle("header__magnify--hidden");
+  mainSearch.addEventListener("focus", search, false);
+  mainSearch.addEventListener("blur", search, false);
+
+  headerSearch.addEventListener("focus", columnSearch, false);
+  headerSearch.addEventListener("blur", columnSearch, false);
+
+  function resizeMenu() {
+    menu.classList.toggle("main-nav--hidden");  
+    btnOpen.classList.toggle("header-toggle__img--hidden");
+    btnClosed.classList.toggle("header-toggle__img--hidden");
+  } 
+
+  function menuTablet() {
+    toDo.classList.toggle("hidden");
+    headerToggle.classList.toggle("header-toggle--big");
   }
-  mainSearch.onblur = function() {
-    headerMagnify.classList.toggle("header__magnify--hidden");
+
+  function search() {
+    Magnify.classList.toggle("header__magnify--hidden");
   }
-  headerSearch.onfocus = function() {
+
+  function columnSearch() {
     formOk.classList.toggle("form__ok--hidden");
     formClosed.classList.toggle("form__closed--hidden");
   }
-  headerSearch.onblur = function() {
-    formOk.classList.toggle("form__ok--hidden");
-    formClosed.classList.toggle("form__closed--hidden");
-  }
-
 }
